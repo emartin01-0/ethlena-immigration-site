34469ea0cfce3239b391c3e53b811f800c8fb5a3
emartin01-0 <ethlenamacauley@gmail.com>
2025-05-03 23:06:40 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Ethlena Macauley | Immigration Attorney",
+  description: "Compassionate and experienced immigration attorney offering personalized virtual legal services.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>{children}</body>
+    </html>
+  );
+}
